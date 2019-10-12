5c2cabbb1709e3ebcc613b8b5ae99dbaad5bc58a
HP_DC7900_small <HP_DC7900_small@AZ218B>
2019-10-12 10:33:01 +0300

    О да, теперь оно нормальное!)

--- src/clas/Thrd.java
+++ src/clas/Thrd.java
@@ -1,5 +1,4 @@
 package clas;
-
 import static clas.Frame.isApressed;
 import static clas.Frame.isDpressed;
 import static clas.Frame.isSpressed;
@@ -8,36 +7,26 @@ import static clas.Init.GameScreen;
 import static clas.Init.ScreenBounds;
 import static clas.Init.ScreenPos;
 import static clas.Init.StepLength;
-import java.io.IOException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import static rendering.Rendering.RenderAll;
-import static rendering.Rendering.RenderGround;
+import static rendering.Rendering.RenderAll2;
 
-public class Thrd extends Thread {
+public class Thrd extends Thread{
 
  @Override
- public void run() {
-  
-   RenderGround();
-  
-  while (true) {
+ public void run(){
+  while(true){
    Sleep(10);
-   
-if (ScreenPos[1] - StepLength >= 0 && isWpressed)ScreenPos[1] -= StepLength;
-if (ScreenPos[0] - StepLength >= 0 && isApressed)ScreenPos[0] -= StepLength;
-if (ScreenPos[1] + StepLength <= GameScreen[1] - ScreenBounds[1] && isSpressed)ScreenPos[1] += StepLength;
-if (ScreenPos[0] + StepLength <= GameScreen[0] - ScreenBounds[0] && isDpressed)ScreenPos[0] += StepLength;    
- 
-    RenderAll();
-
+   if(ScreenPos[1] - StepLength >= 0 && isWpressed)ScreenPos[1] -= StepLength;
+   if(ScreenPos[0] - StepLength >= 0 && isApressed)ScreenPos[0] -= StepLength;
+   if(ScreenPos[1] + StepLength <= GameScreen[1] - ScreenBounds[1] && isSpressed)ScreenPos[1] += StepLength;
+   if(ScreenPos[0] + StepLength <= GameScreen[0] - ScreenBounds[0] && isDpressed)ScreenPos[0] += StepLength;
+   RenderAll2();
   }
  }
 
- private void Sleep(int l) {
-  try {
+ private void Sleep(int l){
+  try{
    Thread.sleep(l);
-  } catch (InterruptedException i) {
+  }catch(InterruptedException i){
   }
  }
 
